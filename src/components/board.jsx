83dc72c6a57c2e5e4334a3e71a98a4464926f062
youngmin <min050410@gmail.com>
2022-06-22 22:47:32 +0900

    add: 위 아래 오른쪽 이동 구현, fix: 왼쪽 이동중 하나만 이동하는 버그

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const Board = () => {
-  let [content, setContent] = useState([['2', '2', '', ''], ['', '', '', ''], ['', '2', '', ''], ['', '', '', '']]);
+  let [content, setContent] = useState([['2', '2', '2', ''], ['', '2', '', ''], ['', '2', '', ''], ['', '', '2', '']]);
 
   const render = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
 
@@ -45,20 +45,82 @@ const Board = () => {
   }, [content]);
 
 
+  const moveRight = (prev) => {
+    console.log(prev);
+    let cnt = 0;
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 3; j++) {
+        if (prev[i][j] !== '') {
+          cnt = cnt + 1;
+        }
+        prev[i][j] = '';
+      }
+      // 채우기
+      for (let k = 3; k >= 4-cnt; k--) {
+        prev[i][k] = '2';
+      }
+      cnt = 0;
+    }
+    console.log(prev);
+    return prev;
+  }
+  
   const moveLeft = (prev) => {
     console.log(prev);
-    for (let i = 3; i >= 0; i--) {
-      for (let j = 3; j >= 0; j--)
-        // 왼쪽으로 한칸 이동
-        if (j > 0) {
-          if (prev[i][j] !== '' && prev[i][j - 1] === '') {
-            prev[i][j] = '';
-            prev[i][j - 1] = '2';
-          }
+    let cnt = 0;
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 3; j++) {
+        if (prev[i][j] !== '') {
+          cnt = cnt + 1;
         }
+        prev[i][j] = '';
+      }
+      // 채우기
+      for (let k = 0; k < cnt; k++) {
+        prev[i][k] = '2';
+      }
+      cnt = 0;
     }
-    setInterval(() => {
-    }, 1)
+    console.log(prev);
+    return prev;
+  }
+  
+  const moveDown = (prev) => {
+    console.log(prev);
+    let cnt = 0;
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 3; j++) {
+        if (prev[j][i] !== '') {
+          cnt = cnt + 1;
+        }
+        prev[j][i] = '';
+      }
+      // 채우기
+      for (let k = 3; k >= 4-cnt; k--) {
+        prev[k][i] = '2';
+      }
+      cnt = 0;
+    }
+    console.log(prev);
+    return prev;
+  }
+
+  const moveUp = (prev) => {
+    console.log(prev);
+    let cnt = 0;
+    for (let i = 0; i <= 3; i++) {
+      for (let j = 0; j <= 3; j++) {
+        if (prev[j][i] !== '') {
+          cnt = cnt + 1;
+        }
+        prev[j][i] = '';
+      }
+      // 채우기
+      for (let k = 0; k < cnt; k++) {
+        prev[k][i] = '2';
+      }
+      cnt = 0;
+    }
     console.log(prev);
     return prev;
   }
@@ -67,17 +129,18 @@ const Board = () => {
   const keyDown = ({ keyCode }) => {
     switch (keyCode) {
       case 37:
-        // 새로운 주소값 할당으로 re-rendering 
+        // 새로운 주소값 할당으로 re-rendering
+        // 전개 연산자는 1차원 배열에서만 유효
         setContent(prev => moveLeft([[...prev[0]], [...prev[1]], [...prev[2]], [...prev[3]]]));
         break;
       case 38:
-        setContent([['', '', '', '2'], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]);
+        setContent(prev => moveUp([[...prev[0]], [...prev[1]], [...prev[2]], [...prev[3]]]));
         break;
       case 39:
-        setContent([['', '', '', ''], ['', '', '', '2'], ['', '', '', ''], ['', '', '', '']]);
+        setContent(prev => moveRight([[...prev[0]], [...prev[1]], [...prev[2]], [...prev[3]]]));
         break;
       case 40:
-        setContent([['', '', '', ''], ['', '', '', ''], ['', '', '2', ''], ['', '', '', '']]);
+        setContent(prev => moveDown([[...prev[0]], [...prev[1]], [...prev[2]], [...prev[3]]]));
         break;
       default:
         break;
